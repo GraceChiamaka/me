fc0ac9056927b58bea05519426b6d9cec37dd220
Amriesgrace <obieri.grace@gmail.com>
2018-11-09 18:18:25 +0100

    add updated portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
+/* 
+* scripts for activating owl carousle plugin
+* toggling overlays displays
+* 
+*/
+
+
 $(document).ready(function(){
+    const carouselOverlay = () => {
+        $(".item").mouseover(function(e){
+            $(this).find(".portfolio-overlay").show();
+        })
+        $(".item").mouseleave(function(){
+            $(this).find(".portfolio-overlay").hide();
+        })
+    }
+    function portfolioOverlay () {
+        const thumbnail = document.getElementsByClassName('projects');
+        const overlay = document.getElementsByClassName('.portfolio-overlay');
+        $('.projects').mouseover(function(ev) {
+            const target= $(ev.currentTarget).closest('.projects').find('.portfolio-overlay').show();
+        });
+        $('.projects').mouseleave(function(ev) {
+            const target = $(ev.currentTarget).closest('.projects').find('.portfolio-overlay').hide();
+        });
+        
+    }
+    carouselOverlay();
+    portfolioOverlay();
+
     $('.about-carousel').owlCarousel({
-       
         center: true,
-        items:1,
+        items:3,
         autoplay:true,
         loop:true,
         margin:3,
@@ -15,25 +43,21 @@ $(document).ready(function(){
 
     $('.portfolio-carousel').owlCarousel({
         items:3,
-        autoplay:false,
-        loop:false,
-        nav:false,
-        margin:10,
+        autoplay:true,
+        loop:true,
+        center: true,
+        margin:3,
         responsive:{
             400:{
                 items:1
             },
             600:{
                 items:3
+            },
+            767:{
+                items:2
             }
         }
-
     });
-
-    $(".item").mouseover(function(e){
-        $(this).find(".portfolio-overlay").show();
-    })
-    $(".item").mouseleave(function(){
-        $(this).find(".portfolio-overlay").hide();
-    })
+   
 });
